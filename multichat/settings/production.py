382b2f6460c0aeb0825307f88618ca6168bb0ca4
AbdelrahmanRabiee <abdelrahman.sico_931@fci.helwan.edu.eg>
2017-10-03 01:54:53 +0200

    production.py edit

--- multichat/settings/production.py
+++ multichat/settings/production.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'multichat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = { '''
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    '''
+    
 }
 
 
