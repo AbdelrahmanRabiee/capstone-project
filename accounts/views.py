df6c791a02a81ba476adfdbca1716bb399e27ee6
AbdelrahmanRabiee <abdelrahman.sico_931@fci.helwan.edu.eg>
2017-10-03 04:31:52 +0200

    requirements commit

--- accounts/views.py
+++ accounts/views.py
@@ -11,14 +11,14 @@ from django.contrib.auth.models import User
 def login_view(request):
     error = None
     if request.user.is_authenticated:
-        return HttpResponseRedirect("/")
+        return HttpResponseRedirect("/inbox/")
     else:
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect("inbox/")
+            return HttpResponseRedirect("/inbox/")
         else:
             error = 'username or password is incorrect'
 
